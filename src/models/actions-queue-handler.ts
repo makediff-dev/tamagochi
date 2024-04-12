f2de35a2a769b32aef186593119445089028a82b
pup pup <demottra@gmail.com>
2024-04-12 14:23:02 +0300

    feat: ordered actions

---
+++ src/models/actions-queue-handler.ts
@@ -0,0 +1,44 @@
+import { EActionNames } from '@prisma/client';
+import { UseActionService } from 'src/use-action/use-action.service';
+
+type UserQueue = {
+    isHandling: boolean;
+    queue: EActionNames[];
+};
+
+export class ActionsQueueHandler {
+    private usersQueue = new Map<string, UserQueue>();
+    private ctx: UseActionService;
+
+    constructor(ctx: UseActionService) {
+        this.ctx = ctx;
+    }
+
+    private getUserQueue(username: string) {
+        if (!this.usersQueue.has(username)) {
+            this.usersQueue.set(username, { isHandling: false, queue: [] });
+        }
+        return this.usersQueue.get(username);
+    }
+
+    private async handleQueue(username: string, userQueue: UserQueue) {
+        userQueue.isHandling = true;
+        while (userQueue.queue.length > 0) {
+            await this.ctx.useMainAction(userQueue.queue[0], username);
+            userQueue.queue.splice(0, 1);
+        }
+        userQueue.isHandling = false;
+    }
+
+    async addActionToQueue(username: string, action: EActionNames) {
+        const userQueue = this.getUserQueue(username);
+        userQueue.queue.push(action);
+
+        if (!userQueue.isHandling) {
+            await this.handleQueue(username, userQueue);
+            return 'Completed all actions!';
+        }
+
+        return 'Action added to queue!';
+    }
+}
--- src/use-action/use-action.controller.ts
+++ src/use-action/use-action.controller.ts
@@ -3,10 +3,15 @@ import { ApiParam } from '@nestjs/swagger';
 import { EActionNames } from '@prisma/client';
 import { UseActionService } from './use-action.service';
 import { ErrorHandler } from 'src/models/error-handler';
+import { ActionsQueueHandler } from 'src/models/actions-queue-handler';
 
 @Controller('use-actions')
 export class UseActionController {
-    constructor(private useActionService: UseActionService) {}
+    actionsQueueHandler: ActionsQueueHandler;
+
+    constructor(private useActionService: UseActionService) {
+        this.actionsQueueHandler = new ActionsQueueHandler(useActionService);
+    }
 
     @ApiParam({
         name: 'action',
@@ -25,6 +30,6 @@ export class UseActionController {
             return actionNameError;
         }
 
-        return await this.useActionService.useMainAction(action, username);
+        return await this.actionsQueueHandler.addActionToQueue(username, action);
     }
 }
